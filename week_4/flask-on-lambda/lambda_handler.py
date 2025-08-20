b95d2fc348ec77fbbbfc09b828f95c1ffa7e1513
Chavi Satish Jain <cjain@cjainV4JQ6.omnissa.com>
2025-08-20 15:00:09 +0530

    deleting a folder

--- week_4/flask-on-lambda/lambda_handler.py
+++
@@ -1,6 +0,0 @@
-from app import app
-import serverless_wsgi #provided via the lambda layer
- 
-def lambda_handler(event,context):
-    # Translate API Gateway HTTP API event -> WSGI -> Flask
-    return serverless_wsgi.handle_request(app, event, context)
--- week_4/flask-on-lambda/outputs.tf
+++
@@ -1,5 +0,0 @@
-# The base URL (no stage path needed because we use the $default stage)
-output "api_url" {
-  description = "Invoke URL for the HTTP API"
-  value       = aws_apigatewayv2_api.http_api.api_endpoint
-}
