37e332fafae297129a3553abb5b1b5cbf57873b3
Chavi Satish Jain <cjain@cjainV4JQ6.omnissa.com>
2025-08-29 15:39:28 +0530

    verifying

--- week_4/terraform-ec2/backend.tf
+++ week_4/terraform-ec2/backend.tf
@@ -1,6 +1,6 @@
 terraform{
     backend "s3" {
-      bucket = "my-terraform-state-bucket"
+      bucket = "terraform-state-demo-v1"
       key = "ec2/terraform.tfstate"
       region = "ap-south-1"
       encrypt= true
